a5937fa739f269072d092271142d7d5e673fa7ba
AeBiCe <Nikolaoz1997@gmail.com>
2021-01-18 12:00:17 +0100

    base program

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Greeklish
 Renames Greek filenames to "Greeklish"
+
+## How to use
+Enter the directory with the files you wish to rename. The program will then iterate through the directory
+and change filename to greek. Non-greek names will not be affected.
+
+## Known issues
+* No error handling	
+* Letter "ψ" is currently not supported
+* Letter 'θ' is represented by '8' instead of 'th'
+* Files in subfolders are not translated
+
+
---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+
+print('''
+   _____               _    _ _     _     
+  / ____|             | |  | (_)   | |    
+ | |  __ _ __ ___  ___| | _| |_ ___| |__  
+ | | |_ | '__/ _ \/ _ \ |/ / | / __| '_ \ 
+ | |__| | | |  __/  __/   <| | \__ \ | | |
+  \_____|_|  \___|\___|_|\_\_|_|___/_| |_| 
+
+      "Translate Greek to Greeklish"                                   
+
+''')
+
+# Alphabet mapping
+greek = "ΑΆΒΓΔΕΈΖΗΉΘθΙΊΪΚΛΜΝΞΟΌΠΡΣΤΥΎΫΦΧΩΏαάβγδεέζηήιίϊΐκλμνξοόπρσςτυύϋΰφχωώ;" 
+english = "AAVGDEEZII88IIIKLMNXOOPRSTYYYFXOOaavgdeeziiiiiiklmnxooprsstuuuufxoo?"
+
+
+directory = input("Enter directory:")
+os.chdir(directory) # Change to dir to input
+
+files = os.listdir(os.getcwd()) # Get files in directory
+
+
+for file in files:
+    table = file.maketrans(greek, english)  # Map characters
+    os.rename(file,file.translate(table))   # Rename
+
+print("Translation complete!")
+                                          
+                                          
+
+
